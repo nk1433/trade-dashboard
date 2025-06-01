231e15c651612a5e24913e52478cb227000ae018
Nitheshkumar <nitheshkumar.g@vuedata.in>
2025-06-01 20:20:36 +0530

    Introduced Navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import PortfolioForm from "./Components/molicules/PortfolioForm";
-import PortfolioTable from "./Components/molicules/PortfolioTable";
-import AllocationIntentForm from "./Components/molicules/AllocationForm";
-import ClosedPositions from "./Components/molicules/ClosedPositions";
-import PortfolioPerformanceChart from "./Components/molicules/Performance";
-import PositionSizer from "./Components/molicules/PositionSize";
-import DisplayScript from "./Components/molicules/Scripts";
+import Navbar from "./Components/molicules/Navbar";
+
 
 const App = () => {
   return (
-    <Router>
-      <nav style={{ padding: "10px" }}>
-        <Link to="/form" style={{ marginRight: "10px" }}>
-          PF Details
-        </Link>
-        <Link to="/table">PF </Link>
-        <Link to="/allocation">Allocation Calculator </Link>
-        <Link to="/closed">Closed </Link>
-        <Link to="/performance">Performance </Link>
-        <Link to="/quick-sizer">Quick Sizer </Link>
-        <Link to="/tracker">Scripts </Link>
-      </nav>
-      <Routes>
-        <Route path="/form" element={<PortfolioForm />} />
-        <Route path="/table" element={<PortfolioTable />} />
-        <Route path="/allocation" element={<AllocationIntentForm />} />
-        <Route path="/closed" element={<ClosedPositions />}/>
-        <Route path="/performance" element={<PortfolioPerformanceChart />} />
-        <Route path="/quick-sizer" element={<PositionSizer />} />
-        <Route path="/tracker" element={<DisplayScript />} />
-      </Routes>
-    </Router>
+    <Navbar />
   );
 };
 
---
+++ src/Components/molicules/Navbar.jsx
@@ -0,0 +1,49 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import PortfolioForm from "./PortfolioForm";
+import ClosedPositions from "./ClosedPositions";
+import DisplayScript from "./Scripts";
+import Watchlist from "./Watchlist";
+import AllocationIntentForm from "./AllocationForm";
+import PerformanceGraph from "./Performance";
+import { Menu } from "@mui/material";
+
+// Configuration for the routes in JSON format
+const routesConfig = [
+  { path: "/pf-config", component: PortfolioForm, linkText: "Configs" },
+//   { path: "/table", component: PortfolioTable, linkText: "PF" },
+  { path: "/allocation-calculator", component: AllocationIntentForm, linkText: "Allocation Intent" },
+  { path: "/closed", component: ClosedPositions, linkText: "Closed" },
+  { path: "/performance", component: PerformanceGraph, linkText: "Performance" },
+  { path: "/watch-list", component: Watchlist, linkText: "Watch List" },
+  { path: "/search-scripts", component: DisplayScript, linkText: "Scripts" },
+];
+
+const Navbar = () => {
+  return (
+    <Router>
+      <nav style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        {routesConfig.map((route) => (
+          <Link
+            key={route.path}
+            to={route.path}
+            style={{ marginRight: "10px", color: 'black' }}
+          >
+            {route.linkText}
+          </Link>
+        ))}
+      </nav>
+     
+      <Routes>
+        {routesConfig.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={<route.component />}
+          />
+        ))}
+      </Routes>
+    </Router>
+  );
+};
+
+export default Navbar;
