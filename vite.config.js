4cd749116a1115492d5435694025d114e0542486
Nitheshkumar <nitheshkumar.g@vuedata.in>
2025-06-07 14:08:57 +0530

    Linted.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import process from 'process';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
