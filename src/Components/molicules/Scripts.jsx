bd087e9de248b30703386c582593d742b75679fa
Nitheshkumar <nitheshkumar.g@vuedata.in>
2025-06-08 13:19:30 +0530

    Refactored.

--- src/Components/molicules/Scripts.jsx
+++ src/Components/molicules/Scripts.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Button, Input } from "@mui/material";
 import { useEffect, useState } from "react";
 import scripts from "../../assets/output.json";
 
@@ -8,20 +8,24 @@ const DisplayScript = () => {
     const [trackingScripts, setTrackingScripts] = useState([]);
 
     useEffect(() => {
-        const initialScripts = localStorage.getItem("scripts") ? JSON.parse(localStorage.getItem("scripts")) : [];
-        setTrackingScripts(initialScripts);
-    }, [])
+        setTrackingScripts(localStorage.getItem("scripts")
+            ? JSON.parse(localStorage.getItem("scripts"))
+            : []
+        );
+    }, []);
 
     const performSearch = (searchTerm) => {
         if (searchTerm.trim().length > 3) {
+
             for (let i = 0; i < scripts.length; i++) {
                 const scriptName = scripts[i].name.toLowerCase();
+
                 if (scriptName.includes(searchTerm.toLowerCase())) {
                     setMatchedScripts(prev => [...prev, scripts[i]]);
                 }
             }
         }
-        else{
+        else {
             setMatchedScripts([]);
         }
     };
@@ -36,54 +40,81 @@ const DisplayScript = () => {
             const existingScripts = JSON.parse(items);
             const hasScriptExist = existingScripts.find(script => script.instrument_key === matchedScript.instrument_key);
             const newScripts = hasScriptExist ? existingScripts : [...existingScripts, matchedScript];
+
             localStorage.setItem("scripts", JSON.stringify(newScripts));
             setTrackingScripts(newScripts);
+            setMatchedScripts([]);
         }
     };
 
     const removeScript = (script) => {
         const items = localStorage.getItem("scripts");
+
         if (items) {
             const existingScripts = JSON.parse(items);
             const updatedScripts = existingScripts.filter(s => s.instrument_key !== script.instrument_key);
+
             localStorage.setItem("scripts", JSON.stringify(updatedScripts));
             setTrackingScripts(updatedScripts);
         }
-    }
+    };
 
     return (
         <Box display="flex" gap={2} padding={2}>
             <Box>
-                <input type="text" placeholder="Script name" value={script} onChange={(e) => setScript(e.target.value)} />
-                <button onClick={() => {
+                <Input type="text" placeholder="Script name" value={script} onChange={(e) => setScript(e.target.value)} onKeyDown={(e) => {
+                    console.log(e.key)
+                    if (e.key === 'Enter') {
+                        performSearch(script);
+                        setScript("");
+                    }
+                }} />
+                <Button onClick={() => {
                     performSearch(script);
                     setScript("");
-                }}>Search</button>
+                }}>
+                    Search
+                </Button>
                 {
                     matchedScripts.map((script) => {
                         const { name, instrument_key } = script;
 
-                        return name ? <li>
-                            <span>{name}</span>
-                            <span> - </span>
-                            <span>{instrument_key}</span>
-                            <button onClick={() => storeScript(script)}>Track</button>
-                        </li> : <></>
+                        return name
+                            ? (
+                                <li key={instrument_key}>
+                                    <span>{name}</span>
+                                    <span> - </span>
+                                    <span>{instrument_key}</span>
+                                    <Button onClick={() => {
+                                        storeScript(script);
+                                    }}>
+                                        Track
+                                    </Button>
+                                </li>
+                            )
+                            : <></>
                     })
                 }
             </Box>
             <Box>
                 {
-                    trackingScripts.length > 0 ? trackingScripts.map((script) => {
-                        const { name, instrument_key } = script;
+                    trackingScripts.length
+                        ? trackingScripts.map(({ name, instrument_key }) => {
 
-                        return name ? <li key={instrument_key}>
-                            <span>{name}</span>
-                            <span> - </span>
-                            <span>{instrument_key}</span>
-                            <button onClick={() => removeScript(script)}>Untrack</button>
-                        </li> : <></>
-                    }) : <p>No scripts tracked yet.</p>
+                            return name
+                                ? (
+                                    <li key={instrument_key}>
+                                        <span>{name}</span>
+                                        <span> - </span>
+                                        <span>{instrument_key}</span>
+                                        <Button onClick={() => removeScript(script)}>
+                                            Untrack
+                                        </Button>
+                                    </li>
+                                )
+                                : <></>
+                        })
+                        : <p>No scripts tracked yet.</p>
                 }
             </Box>
         </Box>
