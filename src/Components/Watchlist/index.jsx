a5ff7b154e490df1b13ac74f79d48baa5a3aab17
nk1433 <iamnk1433@gmail.com>
2025-08-30 18:52:39 +0530

    Refactored.

--- src/Components/Watchlist/index.jsx
+++ src/Components/Watchlist/index.jsx
@@ -1,30 +1,12 @@
-import { useEffect } from 'react';
 import { Box } from '@mui/material';
-import { useDispatch, useSelector } from 'react-redux';
-import Refresh from './Refresh';
+import {  useSelector } from 'react-redux';
 import WatchList from './Table';
-import { calculateMetricsForScript } from '../../Store/upstoxs';
-
-const onSubmit = (scripts) => {
-  return calculateMetricsForScript(scripts)
-};
 
 const Dashboard = () => {
-  const dispatch = useDispatch();
-  const scripts = localStorage.getItem('scripts') ? JSON.parse(localStorage.getItem('scripts')) : [];
-  const {
-    portfolioSize,
-    riskPercentage: riskPercentageOfPortfolio
-  } = useSelector((state) => state.portfolio);
   const { orderMetrics } = useSelector((state) => state.orders);
-
-  useEffect(() => {
-    dispatch(onSubmit(scripts));
-  }, [portfolioSize, riskPercentageOfPortfolio]);
 
   return (
     <Box>
-      <Refresh refreshScripts={() => onSubmit(scripts)} />
       <WatchList scripts={orderMetrics} />
     </Box>
   );
